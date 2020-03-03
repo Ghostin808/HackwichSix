c35e7318fa6d4e296b37bc2c2980da8b4a7b21db
Joseph Payongayong <Jp47@hawaii.edu>
2020-03-03 10:47:33 -1000

    Problem Set 1 completed

--- Joseph Payongayong/ViewController.swift
+++ Joseph Payongayong/ViewController.swift
@@ -11,19 +11,20 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var myFriends = ["Ben", "Julian", "Joey"]
+    var countries = ["Tokyo","Toronto","Athens"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return myFriends.count
+         return countries.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
-        let text = myFriends[indexPath.row]
+        let text = countries[indexPath.row]
         cell?.textLabel?.text = text
         return cell!
     }
+
     
-var countries = ["Tokyo","Toronto","Athens"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
